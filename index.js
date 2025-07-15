244107952f82dd67420e9674a56332389b02350d
Patrick <Patrick@nytyler.com>
2025-07-15 15:12:30 -0400

    Updated to work with new base route and added breadcrumbs

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
-const DOMAIN_NAME = "http://localhost:3000/"
+const DOMAIN_NAME = "http://localhost:3000/" // where classy is running
+const API_ROOT = DOMAIN_NAME + "get/"
+
 const GITHUB_ORIGIN = "https://pjt727.github.io"
 const WINDOW_NAME = window.location.origin == GITHUB_ORIGIN ? `${GITHUB_ORIGIN}/classy-api` : window.location.origin
+const ROUTE_LINK = `${windowName}?route=`
 document.addEventListener("DOMContentLoaded", async () => {
     const searchParams = new URLSearchParams(window.location.search);
     let routeValue = searchParams.get("route")
     routeValue = routeValue === null ? "" : routeValue
     const domainName = document.getElementById("domain-name")
 
-    domainName.innerText = DOMAIN_NAME
+    domainName.innerText = API_ROOT
     const route = document.getElementById("route")
     route.value = routeValue
+
+    // add bread crumbs
+    let breadCrumbElement = document.getElementById("breadcrumbs")
+    let currentBreadcrumbHref = ROUTE_LINK
+    let breadcrumbLink = document.createElement("a")
+    breadcrumbLink.href = currentBreadcrumbHref
+    breadcrumbLink.innerText = "/"
+    breadCrumbElement.appendChild(breadcrumbLink)
+
+    let routeParts = (routeValue + "/").split("/");
+    for (let i = 0; i < routeParts.length - 1; i++) {
+        let routePart = routeParts[i];
+        let sep = document.createElement("span")
+        sep.innerText = ">"
+        sep.style.display = "inline"
+        breadCrumbElement.appendChild(sep)
+        currentBreadcrumbHref += routePart
+        let breadcrumbLink = document.createElement("a")
+        breadcrumbLink.href = currentBreadcrumbHref
+        breadcrumbLink.innerText = routePart
+        breadCrumbElement.appendChild(breadcrumbLink)
+        currentBreadcrumbHref += "/"
+    }
+
+
+    // remove the API ROOT prefix when inserting into the text box
     route.addEventListener("input", () => {
-        console.log("changed")
-        if (route.value.startsWith(DOMAIN_NAME)) {
-            console.log("changed")
+        if (route.value.startsWith(API_ROOT)) {
             let text = route.value
-            route.value = text.slice(DOMAIN_NAME.length)
+            route.value = text.slice(API_ROOT.length)
         }
     })
+
+    // reload the page listener
     const changeWindow = () => window.location.href = `${WINDOW_NAME}/?route=${route.value}`
     route.addEventListener("keypress", (event) => {
         if (event.key === 'Enter') {
@@ -25,13 +54,15 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
     })
 
+
     route.focus()
+    // get the data from the api
     const out = document.getElementById('output')
     try {
-        const response = await fetch(DOMAIN_NAME + routeValue)
-        // if (!response.ok) {
-        //     throw new Error('Failed to fetch data');
-        // }
+        const response = await fetch(API_ROOT + routeValue)
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
 
         const data = await response.json()
         inject_links(routeValue, data)
@@ -49,6 +80,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         updateData()
         window.addEventListener('resize', debounce(updateData, 100));
     } catch (error) {
+        out.innerHTML = `Failed to get api data ${error}`
         console.error(error);
     }
 })
@@ -65,10 +97,11 @@ function debounce(func, delay) {
     };
 }
 
+// TODO: add more injection links
 function inject_links(route, json_data) {
     if (/^[a-zA-Z0-9]+\/courses$/.test(route)) {
         json_data.forEach((course) => {
-            const newRoute = `${route}${course.subject_code}/${course.number}`
+            const newRoute = `${route}/${course.subject_code}/${course.number}`
             course._course_link = `${WINDOW_NAME}?route=${newRoute}`
         })
     } else if (/^[a-zA-Z0-9]+\/[a-zA-Z0-9]+\/classes$/.test(route)) {
